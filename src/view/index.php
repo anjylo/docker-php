4c08c10eb869b56d300323a789e5437d452de319
angelo <gelo.guimba@gmail.com>
2022-11-27 23:01:56 +0800

    Add sample mvc integration

---
+++ src/view/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <div style="
+        display: flex; 
+        justify-content: center; 
+        align-items: center;
+        color: #36454F;
+        text-transform: uppercase;
+        height: 90vh;
+        font-family: Verdana, Geneva, Tahoma, sans-serif
+    ">
+        <h1><?= $message ?></h1>
+    </div>
+</body>
+</html>
